1ba48fdc5039f0ab0c3eaa680e0a8da9cbf1efbe
Allan Shrestha <stevevai111@hotmail.com>
2022-03-09 22:58:03 +0000

    Styling done for Form page

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
-import Button from '../Button/Button';
-
+import React from "react";
+import Button from "../Button/Button";
+import "./Form.scss";
 
 const Form = () => {
-
   return (
     <div>
-      <form action="">
+      <form action="" className="form">
         <h1>Add your course</h1>
-        <label htmlFor="for name">Name:</label>
-        <input type="search" name="location"/>
-        <label htmlFor="for location">Location:</label>
-        <input type="search" name="location"/>
-        <label htmlFor="for price">Price:</label>
-        <input type="search" />
-        <label htmlFor="for duration">Duration:</label>
-        <input type="search" />
-        <label htmlFor="for summary">Summary:</label>
-        <input type="search" />
-      
-      <Button buttonText={"Cancel"} className='button'/>
-      <Button buttonText={"Save"} className='button'/>
+        <div className="form-input">
+          <div>
+            <label htmlFor="for name">Name:</label>
+            <input type="search" name="Name" />
+          </div>
 
+          <div>
+            <label htmlFor="for location">Location:</label>
+            <input type="search" name="location" />
+          </div>
+
+          <div>
+            <label htmlFor="for price">Price:</label>
+            <input type="search" name="price" />
+          </div>
+
+          <div>
+            <label htmlFor="for duration">Duration:</label>
+            <input type="search" name="duration" />
+          </div>
+
+          <div>
+            <label htmlFor="for summary">Summary:</label>
+            <input type="search" name="summary" />
+          </div>
+        </div>
+
+        <div className="button-box">
+          <Button buttonText={"Cancel"} className="button" />
+          <Button buttonText={"Save"} className="button" />
+        </div>
+        
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Form;
